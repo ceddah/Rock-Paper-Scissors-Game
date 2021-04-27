b24996b564e96562d6becf7b74ff1f1da48a61e9
ceddah <cedanik1@gmail.com>
2021-04-27 18:38:09 +0200

    RPS-Game

--- script.js
+++ script.js
@@ -18,19 +18,30 @@ playBtn.addEventListener('click', () => {
     screens[0].classList.add('up');
 })
 
-playerOptionBtns.forEach(btn => btn.addEventListener('click', (e) => {
-    playerOption(e)
-}));
+//MAKE SURE THAT WE CANT GET TIED........
+//MAKE SURE THAT WE CANT GET TIED........
+//MAKE SURE THAT WE CANT GET TIED........
+//MAKE SURE THAT WE CANT GET TIED........
+//MAKE SURE THAT WE CANT GET TIED........
+
+screens[1].addEventListener('click', (e) => {
+    if(e.target.classList.contains('choose-RPS')) {
+        playerOption(e)
+    }
+})
 
 //Functions
 function computerPlay() {
     computerChoose = options[Math.floor(Math.random() * 3)];
+    if(computerChoose == playerChoose) {
+        return computerPlay();
+    }
 }
 
 function playerOption(e) {
     computerPlay();
     playerChoose = e.target.querySelector('p').innerText.toLowerCase();
-    playRPS(computerChoose, playerChoose);
+    showResults(computerChoose, playerChoose);
 }
 
 
@@ -53,6 +64,143 @@ function createElements() {
 }
 
 
-function playRPS(computerSelection, playerSelection) {
-    console.log(computerSelection, playerSelection)
+function showResults(computerSelection, playerSelection) {
+    console.log(computerSelection, playerSelection);
+    const computerDiv = [...playerOptionBtns].filter(div => {
+        const value = div.querySelector('p').innerText.toLowerCase();
+        if(value === computerSelection) return div;
+    })
+
+    const playerDiv = [...playerOptionBtns].filter(div => {
+        const value = div.querySelector('p').innerText.toLowerCase();
+        if(value === playerSelection) return div
+    })
+
+    //Now we will delete all choices and show both options and conclude winner.
+    choicesEl.innerHTML = '';
+
+    if(computerSelection === playerSelection) {
+        const playerDivEl = document.createElement('li');
+        playerDivEl.appendChild(playerDiv[0])
+        choicesEl.appendChild(playerDivEl);
+
+        //Messange on who won.
+        let result = decideWinner(computerSelection, playerSelection);
+        const messageEl = document.createElement('li');
+        const message = document.createElement('h2');
+        message.innerText = result;
+        messageEl.appendChild(message);
+        choicesEl.appendChild(messageEl);
+
+        //Original code only had else statement and I expeted to insert both Divs in, but since I only had
+        //a reference to a player Choice DIV and Computer Choice DIV, if we got tied then code would break
+        //because i couldn't insert same div in two places, so i made new DIV that has same stats as fist one.
+        const computerDivEl = document.createElement('li');
+        const tiedDiv = document.createElement('button');
+        tiedDiv.setAttribute('class', 'choose-RPS');
+        const pTag = document.createElement('p')
+        pTag.innerText = playerDiv[0].querySelector('p').innerText;
+        const img = document.createElement('img');
+        img.setAttribute('src', playerDiv[0].querySelector('img').src);
+        tiedDiv.appendChild(pTag);
+        tiedDiv.appendChild(img);
+        computerDivEl.appendChild(tiedDiv);
+        choicesEl.appendChild(computerDivEl);
+
+    } else {
+        const playerDivEl = document.createElement('li');
+        playerDivEl.appendChild(playerDiv[0])
+        choicesEl.appendChild(playerDivEl);
+
+        //Messange on who won.
+        let result = decideWinner(computerSelection, playerSelection);
+        const messageEl = document.createElement('li');
+        const message = document.createElement('h2');
+        message.innerText = result;
+        messageEl.appendChild(message);
+        choicesEl.appendChild(messageEl);
+
+        const computerDivEl = document.createElement('li');
+        computerDivEl.appendChild(computerDiv[0]);
+        choicesEl.appendChild(computerDivEl);
+
+        //Increasing the score..
+        increaseScore(result);
+    }
+
+    //Playing Again after round is over.
+    screens[1].classList.add('between-rounds');
+    setTimeout(() => {
+        choicesEl.querySelectorAll('li').forEach(li => choicesEl.removeChild(li));
+        createElements();
+        //remove user-select
+        screens[1].classList.remove('between-rounds');
+    }, 5000)
+
+    
+}
+
+function decideWinner(computer, player) {
+    let result;
+    if(computer === 'rock') {
+        if(player === 'paper') {
+            result = 'You won!'
+        } else if(player === 'scissors') {
+            result = 'You Lost.'
+        } else {
+            result = 'You are tied.'
+        }
+    } else if(computer === 'paper') {
+        if(player === 'rock') {
+            result = 'You Lost.'
+        } else if(player === 'scissors') {
+            result = 'You Won!'
+        } else {
+            result = 'You are tied.'
+        }
+    } else if(computer === 'scissors') {
+        if(player === 'paper') {
+            result = 'You Lost.'
+        } else if(player === 'rock') {
+            result = 'You Won!'
+        } else {
+            result = 'You are Tied';
+        }
+    }
+    return result;
+}
+
+function increaseScore(result) {
+    if(result == 'You Lost.') {
+        computerScore++;
+    } else if(result == 'You Won!') {
+        playerScore++
+    } else {
+        computerScore = computerScore;
+        playerScore = playerScore;
+    }
+    playerScoreEl.innerHTML = `Player: ${playerScore}`;
+    computerScoreEl.innerHTML = `Computer: ${computerScore}`;
+    if(computerScore === 5)  {
+        gameOverScreen('Computer');
+    }else if(playerScore === 5) {
+        gameOverScreen('Player');
+    }
+}
+
+function gameOverScreen(winner) {
+    screens[1].classList.remove('between-rounds');
+    if(winner === 'Computer') {
+        screens[1].innerHTML = `
+        <div class="title">
+            <h2 class="you-lost" >You lost.. Better luck next time.</h2>
+        </div>
+        <button class="game-over" onClick="location.reload(true)">Try Again</button>`
+    } else {
+        screens[1].innerHTML = `
+        <div class="title">
+            <h2 class="you-won" >Congratulations, you won the game!</h2>
+        </div>
+        <button class="game-over" onClick="location.reload(true)">Play Again</button>`
+    }
 }
